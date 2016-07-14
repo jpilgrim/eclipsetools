508346b7d5afd7d013fbc74c4f074ed61beaddd0
Jens von Pilgrim <Jens.von.Pilgrim@numberfour.eu>
2016-07-14 20:00:18 +0200

    j2og: created feature j2og emf

--- j2og/plugins/de.jevopi.j2og.emf/src/de/jevopi/j2og/emf/model/EMFModelCreator.java
+++ j2og/plugins/de.jevopi.j2og.emf/src/de/jevopi/j2og/emf/model/EMFModelCreator.java
@@ -10,7 +10,6 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
